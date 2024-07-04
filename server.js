cb4d4cb9f16f6d117edace8615387234e4eaa7c8
JoseA Sanchez <meldos220@gmail.com>
2024-07-04 16:22:52 -0600

    Navigation Ready

--- server.js
+++ server.js
@@ -2,16 +2,37 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
+const PUBLIC = path.join(__dirname, 'public');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(PUBLIC));
 
 app.get('/', (req, res) => {
-    console.log('Loading home...');
-    res.send('Hello World');
+    console.log('Loading home...\n');
+    res.sendFile(path.join(PUBLIC, 'home.html'));
 });
 
-app.listen(port, () => {
-    console.log('Server is running on port ' + port);
+app.get('/contactus', (req, res) => {
+    console.log('Loading contact us...');
+    res.sendFile(path.join(PUBLIC, 'contactus.html'));
+});
+
+app.post('/contactus', (req, res) => {
+    console.info('Contact Us has been called...\n');
+    const name = req.body.name;
+    const email = req.body.email;
+    const subject = req.body.subject;
+
+    console.log('Form data\n');
+    console.log('Name: ' + name);
+    console.log('Email: ' + email);
+    console.log('Subject: ' + subject);
+
+    res.redirect('/');
+});
+
+app.listen(PORT, () => {
+    console.log('🥑 Server is running on port ' + PORT);
 });
 
